ea2bd6406c83bc5f8629f7c4431c0c5a52023f14
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-21 19:50:43 +0000

    correct misspelling in rule message

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ COLORS = {
 
 cprint(' Welcome to the Hangman Game!\n', COLORS["YELLOW"])
 cprint(' Rules:\n', COLORS["GREEN"])
-cprint(' * We generate a random word\n * You guess a letter\n * If the letter is in the word, the man lives a little longer\n * If the ltter is not in the word he gets closer to be hanged\n * If you find all words, you win and the man gets to live\n * You have 9 failed attempts, otherwise he gets hanged.\n, COLORS["RED"])
+cprint(' * We generate a random word\n * You guess a letter\n * If the letter is in the word, the man lives a little longer\n * If the ltter is not in the word he gets closer to be hanged\n * If you find all words, you win and the man gets to live\n * You have 9 failed attempts, otherwise he gets hanged.\n', COLORS["RED"])
 cprint(' Enjoy!\n', COLORS["GREEN"])
 
 cprint(logo, COLORS["YELLOW"])
